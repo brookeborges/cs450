0f33eb59de2b6c3d0959bbd0c3f7cb69e0fce907
Brooke Ann Borges <bborges@blue.cs.sonoma.edu>
2017-03-09 23:02:50 -0800

    Simple commands with no redirection implemented

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "constants.h"
 #include "parsetools.h"
 #include <sys/wait.h>
+#include <unistd.h>
+
 
 int main() {
     // Buffer for reading one line of input
@@ -10,14 +12,19 @@ int main() {
 
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
+    fork();
     while( fgets(line, MAX_LINE_CHARS, stdin) ) {
         int num_words = split_cmd_line(line, line_words);
-
+        int a = fork();
         // Just for demonstration purposes
+        if (a == 0){
         for (int i=0; i < num_words; i++)
             printf("%s\n", line_words[i]);
+        execvp(line_words[0],line_words);
+}
 
     }
+    printf("here\n");
 
     return 0;
 }
